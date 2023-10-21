fe4d10851e1ecaaf4c92c930d4fd1d778c9567b0
DESKTOP-RSQRIA7\User <alexander.goyens@gmail.com>
2023-10-21 09:21:05 +0200

    work_orders migration, model + controller

---
+++ app/Http/Controllers/WorkOrderController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\WorkOrder;
+
+class WorkOrderController extends Controller
+{
+    // Index: Show list of work orders
+    public function index()
+    {
+        $workOrders = WorkOrder::orderBy('start_date', 'desc')->get();
+        return view('workorders.index', compact('workOrders'));
+    }
+
+    // Create: Show work order creation form
+    public function create()
+    {
+        return view('workorders.create');
+    }
+
+    // Store: Save new work order
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'start_date' => 'required|date',
+            'end_date' => 'required|date',
+            'employee_name' => 'required|string',
+            'notes' => 'nullable|string',
+            'status' => 'required|in:open,completed,other',
+            'image_paths' => 'nullable|string',
+        ]);
+
+        $workOrder = WorkOrder::create($validatedData);
+
+        // Handle image uploads
+
+        return redirect()->route('workorders.index')->with('success', 'Work order created successfully.');
+    }
+
+    // Show: Display specific work order
+    public function show(WorkOrder $workOrder)
+    {
+        return view('workorders.show', compact('workOrder'));
+    }
+
+    // Other methods: Update, edit, delete, etc.
+}
---
+++ database/migrations/2023_10_21_071819_create_work_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('work_orders', function (Blueprint $table) {
+            $table->id(); // A_I primary key
+            $table->dateTime('start_date'); // start date and time
+            $table->dateTime('end_date'); // end date and time
+            $table->string('employee_name'); // employee
+            $table->text('notes')->nullable(); // notes
+            $table->enum('status', ['open', 'completed', 'other'])->default('open'); // status of order
+            $table->text('image_paths')->nullable(); // image paths (multiple paths JSON or serialized data)
+            $table->timestamps(); // created_at and updated_at
+
+            $table->index(['start_date', 'end_date']); // Index for date range queries
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('work_orders');
+    }
+};
