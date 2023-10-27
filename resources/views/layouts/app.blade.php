c51a95d21d59406ddaf8d7e9ed7db8274676ae58
DESKTOP-RSQRIA7\User <alexander.goyens@gmail.com>
2023-10-27 10:42:26 +0200

    footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 <body>
     <header>
         <!-- header content -->
-        <nav>
+        <nav class="teal">
             <ul>
                 <li><a href="{{ route('workorders.index') }}">All Orders</a></li>
             </ul>
@@ -33,8 +33,23 @@
         @yield('content')
     </main>
 
-    <footer>
+    <footer class="page-footer teal">
         <!-- footer content -->
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text">Footer Content</h5>
+                    <p class="grey-text text-lighten-4">This is your Materialize CSS footer content.</p>
+                </div>
+                <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Links</h5>
+                    <ul>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </footer>
 
     <!-- JavaScript scripts -->
